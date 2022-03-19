b84902b2764de0fef5d0acf85d55c80015422e66
Muhammad Kamkoriwala <kamkoriwalamuhammad@gmail.com>
2022-03-19 17:11:50 +0530

    version 2.54

--- BlogPage/settings.py
+++ BlogPage/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ij+b*ydfhn=63(pn)+!$*62c%u8)npk$2&lx3@q+x@+l^&+o79'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['edgeblogs.herokuapp.com', '127.0.0.1']
 
